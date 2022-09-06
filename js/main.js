4de0acd65729c452fe4163ea64d4ad9fc2047cdd
Israel Castillo <israelocastillo@gmail.com>
2022-09-05 22:05:36 -0500

    js changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 alert('working')
+
+document.querySelector('button').addEventListener('click', apiRequest)
+
+async function apiRequest(){
+    const rapperName = document.querySelector('input').value
+    try{
+        const response = await fetch(`https://xenoblade-heroes-api.herokuapp.com/api/${heroesName}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('.name').innerText = data.name
+        document.querySelector('.age').innerText = data.age
+        document.querySelector('.species').innerText = data.species
+        document.querySelector('.birthPlace').innerText = data.birthPlace
+        document.querySelector('.game').innerText = data.game
+    }catch(error){
+        console.log(error)
+    }
+}
